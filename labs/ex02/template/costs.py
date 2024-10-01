3062686060e4568d9766b99e3a0e2a0100917a86
bcrobin03 <robin.bochatay@gmail.com>
2024-10-01 11:20:35 +0200

    labs 3

--- labs/ex02/template/costs.py
+++ labs/ex02/template/costs.py
@@ -8,9 +8,9 @@ def compute_loss(y, tx, w):
     """Calculate the loss using either MSE or MAE.
 
     Args:
-        y: shape=(N, )
-        tx: shape=(N,2)
-        w: shape=(2,). The vector of model parameters.
+        y: numpy array of shape=(N, )
+        tx: numpy array of shape=(N,2)
+        w: numpy array of shape=(2,). The vector of model parameters.
 
     Returns:
         the value of the loss (a scalar), corresponding to the input parameters w.
@@ -18,5 +18,6 @@ def compute_loss(y, tx, w):
     # ***************************************************
     # INSERT YOUR CODE HERE
     # TODO: compute loss by MSE
+    L = np.sum((y - tx @ w)**2)
     # ***************************************************
-    raise NotImplementedError
+    return L
